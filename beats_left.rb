a7bf6d8fb7af802314fe173e40dfd3d2e7941c8c
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-19 20:38:45 +0000

    automatically backed up by learn

--- beats_left.rb
+++ beats_left.rb
@@ -2,9 +2,9 @@ def dance
   beats_left_in_song = 60
   
   loop do
-  if beats_left_in_song == 0
-    break
-  end
+    if beats_left_in_song == 0
+      break
+    end
   
   puts "slide to left"
   sleep(1)
@@ -15,5 +15,6 @@ def dance
   
   beats_left_in_song = beats_left_in_song - 10
   puts "Congratulations! You're done with #{beats_left_in_song}!"
+end
 end
 dance
