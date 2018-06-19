92cc79828328fefa5df51c0e3362ca2560ec4972
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-19 20:28:53 +0000

    automatically backed up by learn

--- counter_loops.rb
+++ counter_loops.rb
@@ -5,11 +5,11 @@ def cha_cha_slide
       break
     end
     puts "slide to left"
-    sleep(0.1)
+    sleep(1)
     puts "slide to right"
-    sleep(0.1)
+    sleep(1)
     puts "and kick"
-    sleep(0.1)
+    sleep(1)
 moves_completed = moves_completed + 1
 puts "You've completed #{moves_completed}!"
 end
