9b3660911d7285e85bb12fe74a9712829122b7f2
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-19 20:36:32 +0000

    automatically backed up by learn

---
+++ beats_left.rb
@@ -0,0 +1,18 @@
+def dance 
+  beats_left_in_song = 60
+  
+  loop do
+  if beats_left_in_song == 0
+    break
+  end
+  
+  puts "slide to left"
+  sleep(1)
+  puts "slide to right"
+  sleep(1)
+  puts "now kick"
+  sleep(1)
+  
+  beats_left_in_song = beats_left_in_song - 10
+end
+dance
